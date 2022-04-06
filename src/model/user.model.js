aa2122df60cdd7550513b38855580754b3630e0d
Sachintha Muramudalige <sachintha@ogmo.io>
2022-04-07 00:34:37 +0530

    user role added

--- src/model/user.model.js
+++ src/model/user.model.js
@@ -5,6 +5,7 @@ const UserSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     mobileNumber: { type: String, required: true },
     password: { type: String, required: true },
+    role: { type: String, required: true }
 });
 
 const User = mongoose.model('user', UserSchema);
